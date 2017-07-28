05743dfdf611ee612ec80048e74fcc7169fa2961
agoalofalife <agoalofalife@gmail.com>
2017-07-28 17:41:26 +0300

    fix app config file

--- config/app.php
+++ config/app.php
@@ -2,18 +2,25 @@
 
 return [
     'services' => [
-        Cart\ServiceProviders\DatabaseServiceProviders::class
+        Cart\ServiceProviders\DatabaseServiceProviders::class,
+        \Cart\ServiceProviders\RedisServiceProvider::class,
     ],
     'drivers' => [
         'database' => [
             'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => 'database',
-            'username'  => 'root',
-            'password'  => 'password',
+            'database'  => 'test',
+            'username'  => 'test',
+            'password'  => 'test',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
+            'prefix'    => ''
+        ],
+        'redis' => [
+            'prefix' => 'cart',
+            'scheme' => 'tcp',
+            'host'   => '127.0.0.1',
+            'port'   => 6379,
         ]
     ],
     'storage' => \Cart\Drivers\DatabaseDriver::class
