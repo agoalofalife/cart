9db031e713b63cea80a034b51971b2afe6edec48
agoalofalife <agoalofalife@gmail.com>
2017-07-31 10:26:12 +0300

    add test helpers functions

---
+++ tests/Unit/HelpersTest.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+namespace Cart\Tests\Unit;
+
+use Cart\Kernel;
+use Cart\Tests\TestCase;
+use Illuminate\Config\Repository;
+
+class HelpersTest extends TestCase
+{
+    public function setUp()
+    {
+        $kernel = new Kernel();
+        $kernel->bootstrapping();
+    }
+
+    public function testConfig() : void
+    {
+        $this->assertInstanceOf(Repository::class, config());
+        config(['test']);
+    }
+}
