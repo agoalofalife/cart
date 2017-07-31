6350a449f7ecaefd3da06131222fa4d221ae3787
agoalofalife <agoalofalife@gmail.com>
2017-07-31 10:28:41 +0300

    add test fix discount

--- tests/Unit/DiscountStrategy/FixDiscountStrategyTest.php
+++ tests/Unit/DiscountStrategy/FixDiscountStrategyTest.php
@@ -7,10 +7,20 @@ use Cart\Tests\TestCase;
 
 class FixDiscountStrategyTest extends TestCase
 {
-    public function testMake() : void
+    public function testMakeGreaterZero() : void
     {
         $sign  = $this->faker()->randomDigit;
         $base  = $this->faker()->randomDigit;
+
+        $class = new FixDiscountStrategy($sign);
+        $result = ($base - $sign);
+        $this->assertEquals($result < 0 ? 0 : $result, $class->make($base));
+    }
+
+    public function testMakeLessZero() : void
+    {
+        $sign  = $this->faker()->randomDigit;
+        $base  = 0;
 
         $class = new FixDiscountStrategy($sign);
         $result = ($base - $sign);
